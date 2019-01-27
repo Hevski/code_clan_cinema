b8274f364dfa5063f9edf1bb271797818d4c0049
Heather <heathercdavies@gmail.com>
2019-01-27 18:11:31 +0000

    final try of most pop time method - still not working

--- models/screening.rb
+++ models/screening.rb
@@ -49,13 +49,10 @@ class Screening
     sql = "SELECT screening_id,
            COUNT(screening_id) AS VALUE_OCCURRENCE
            FROM tickets GROUP BY screening_id
-           WHERE film_id = $1
            ORDER BY VALUE_OCCURRENCE DESC"
-    # values = [@id]
-    # pop_time = SqlRunner.run(sql, values).first
-    # return pop_time.map{ |time| Screening.new(time) }
-    values = [@id]
-    result = SqlRunner.run(sql, values).first
+    result = SqlRunner.run(sql)['screening_id'][0].to_i
+    #binding.pry
     return result
   end
+  #how do i get pry to work?
 end
